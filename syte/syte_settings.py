c560014089aa95d8a62e5e1c672d7e959ea02fe9
Keith Entzeroth <keith@Keith-Entzeroths-MacBook-Pro.local>
2012-06-02 10:11:32 -0500

    initial syte checkin

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -2,20 +2,6 @@
 DEPLOYMENT_MODE = 'dev'
 COMPRESS_REVISION_NUMBER = '1.0'
 
-#Blog Integration: Tumblr
-TUMBLR_BLOG_URL = '[ENTER TUMBLR BLOG URL] ex. rigoneri.tumblr.com'
-TUMBLR_API_URL = 'http://api.tumblr.com/v2/blog/{0}'.format(TUMBLR_BLOG_URL)
-TUMBLR_API_KEY = '[ENTER TUMBLR API KEY HERE, SEE TUMBLR SETUP INSTRUCTIONS]'
-
-
-#Twitter Integration
-TWITTER_INTEGRATION_ENABLED = True
-TWITTER_API_URL = 'http://api.twitter.com/1/statuses/user_timeline.json?include_rts=false&exclude_replies=true&screen_name='
-TWITTER_CONSUMER_KEY = '[ENTER TWITTER CONSUMER KEY HERE, SEE TWITTER SETUP INSTRUCTIONS]'
-TWITTER_CONSUMER_SECRET = '[ENTER TWITTER CONSUMER SECRET HERE, SEE TWITTER SETUP INSTRUCTIONS]'
-TWITTER_USER_KEY = '[ENTER TWITTER USER KEY HERE, SEE TWITTER SETUP INSTRUCTIONS]'
-TWITTER_USER_SECRET = '[ENTER TWITTER USER SECRET HERE, SEE TWITTER SETUP INSTRUCTIONS]'
-
 
 #Github Integration
 GITHUB_INTEGRATION_ENABLED = True
@@ -24,13 +10,14 @@ GITHUB_REPOS_API_URL = 'https://github.com/api/v2/json/repos/show/'
 
 
 #Dribbble Integration
-DRIBBBLE_INTEGRATION_ENABLED = True
+DRIBBBLE_INTEGRATION_ENABLED = False 
 DRIBBBLE_API_URL = 'http://api.dribbble.com/players/'
 
 
 if DEPLOYMENT_MODE == 'dev':
     SITE_ROOT_URI = 'http://127.0.0.1:8000/'
     DEBUG = True
+    from local_settings import *
 else:
     DEBUG = False
     SITE_ROOT_URI = '[ENTER PROD URL HERE] ex. http://rigoneri.herokuapp.com/'
